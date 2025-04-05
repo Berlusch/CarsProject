6caf9737edba4ac79b8376ba9687ef4cd47a44ff
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-05 13:22:58 +0200

    DAL update

--- CarsProject.DAL/CarsDBContext.cs
+++ CarsProject.DAL/CarsDBContext.cs
@@ -4,16 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using CarsProject.Model;
 
 namespace CarsProject.DAL
 {
     public class CarsDbContext : DbContext
     {
-        public DbSet<CarMake> CarMakes { get; set; }
-        public DbSet<CarModel> CarModels { get; set; }
-        public DbSet<CarOwner> CarOwners { get; set; }
-        public DbSet<CarRegistration> CarRegistrations { get; set; }
-        public DbSet<CarEngineType> CarEngineTypes { get; set; }
+        public DbSet<CarsProject.Model.CarMake> CarMakes { get; set; }
+        public DbSet<CarsProject.Model.CarModel> CarModels { get; set; }
+        public DbSet<CarsProject.Model.CarOwner> CarOwners { get; set; }
+        public DbSet<CarsProject.Model.CarRegistration> CarRegistrations { get; set; }
+        public DbSet<CarsProject.Model.CarEngineType> CarEngineTypes { get; set; }
 
         public CarsDbContext(DbContextOptions<CarsDbContext> options) : base(options) { }
     }
