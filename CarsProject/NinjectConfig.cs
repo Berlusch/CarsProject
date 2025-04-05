9168f4afac1e82945c4924d4b66a8d281173b5d8
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-05 13:23:08 +0200

    Update NinjectConfig.cs

--- CarsProject/NinjectConfig.cs
+++ CarsProject/NinjectConfig.cs
@@ -1,6 +1,23 @@
-﻿namespace CarsProject
+﻿using Ninject;
+using CarsProject.Repository;
+using CarsProject.Repository.Common;
+
+namespace CarsProject
 {
-    public class NinjectConfig
+    public static class NinjectConfig
     {
+        public static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+            // Repository bindings
+            kernel.Bind<ICarMakeRepository>().To<CarMakeRepository>();
+            kernel.Bind<ICarModelRepository>().To<CarModelRepository>();
+            kernel.Bind<ICarOwnerRepository>().To<CarOwnerRepository>();
+            kernel.Bind<ICarRegistrationRepository>().To<CarRegistrationRepository>();
+            kernel.Bind<ICarEngineTypeRepository>().To<CarEngineTypeRepository>();
+
+            return kernel;
+        }
     }
 }
