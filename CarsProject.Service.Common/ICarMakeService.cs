7d1b7624aed843d347571bb2d487ec459ff8fbf3
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-06 15:16:09 +0200

    CarMake Services

---
+++ CarsProject.Service.Common/ICarMakeService.cs
@@ -0,0 +1,16 @@
+﻿using CarsProject.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CarsProject.Service
+{
+    public interface ICarMakeService
+    {
+        Task<IEnumerable<CarMake>> GetAllCarMakesAsync();
+        Task<CarMake> GetCarMakeByIdAsync(int id);
+        Task<CarMake> AddCarMakeAsync(CarMake carMake);
+        Task<CarMake> UpdateCarMakeAsync(CarMake carMake);
+        Task<bool> DeleteCarMakeAsync(int id);
+    }
+}
+
---
+++ CarsProject.Service/CarMakeService.cs
@@ -0,0 +1,48 @@
+﻿using CarsProject.Model;
+using CarsProject.Repository.Common;
+
+
+namespace CarsProject.Service
+{
+    public class CarMakeService : ICarMakeService
+    {
+        private readonly ICarMakeRepository _carMakeRepository;
+
+        
+        public CarMakeService(ICarMakeRepository carMakeRepository)
+        {
+            _carMakeRepository = carMakeRepository;
+        }
+
+       
+        public async Task<IEnumerable<CarMake>> GetAllCarMakesAsync()
+        {
+            return await _carMakeRepository.GetAllAsync();
+        }
+
+        
+        public async Task<CarMake> GetCarMakeByIdAsync(int id)
+        {
+            return await _carMakeRepository.GetByIdAsync(id);
+        }
+
+       
+        public async Task<CarMake> AddCarMakeAsync(CarMake carMake)
+        {
+            return await _carMakeRepository.AddAsync(carMake);
+        }
+
+       
+        public async Task<CarMake> UpdateCarMakeAsync(CarMake carMake)
+        {
+            return await _carMakeRepository.UpdateAsync(carMake);
+        }
+
+        
+        public async Task<bool> DeleteCarMakeAsync(int id)
+        {
+            return await _carMakeRepository.DeleteAsync(id);
+        }
+    }
+}
+
