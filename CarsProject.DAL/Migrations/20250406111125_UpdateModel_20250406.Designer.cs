0b417a0120375839561a437b42a1e351014d4490
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-06 15:12:26 +0200

    New migrations

--- CarsProject.DAL/Migrations/20250406111125_UpdateModel_20250406.Designer.cs
+++ CarsProject.DAL/Migrations/20250406111125_UpdateModel_20250406.Designer.cs
@@ -4,6 +4,7 @@ using CarsProject.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarsProject.DAL.Migrations
 {
     [DbContext(typeof(CarsDbContext))]
-    partial class CarsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250406111125_UpdateModel_20250406")]
+    partial class UpdateModel_20250406
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
