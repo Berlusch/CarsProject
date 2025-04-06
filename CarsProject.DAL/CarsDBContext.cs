8482b91914462af0915ef7c441ef67eff96e9e44
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-06 15:12:07 +0200

    Update CarsDBContext.cs

--- CarsProject.DAL/CarsDBContext.cs
+++ CarsProject.DAL/CarsDBContext.cs
@@ -18,8 +18,8 @@ namespace CarsProject.DAL
             // 1:n relationships
 
             modelBuilder.Entity<CarModel>().HasOne(g => g.CarMake);           
-            //modelBuilder.Entity<CarModel>().HasOne(g => g.CarEngineType)
-            //.WithMany();  
+            modelBuilder.Entity<CarModel>().HasOne(g => g.CarEngineType)
+            .WithMany();  
             modelBuilder.Entity<CarRegistration>().HasOne(g => g.CarOwner);
             modelBuilder.Entity<CarRegistration>().HasOne(g => g.CarModel);
 
