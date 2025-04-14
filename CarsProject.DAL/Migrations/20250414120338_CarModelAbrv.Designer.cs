89b3aff04e5bf0b637272c5a4241ab27ff90d580
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-14 14:22:05 +0200

    New migration

--- CarsProject.DAL/Migrations/20250414120338_CarModelAbrv.Designer.cs
+++ CarsProject.DAL/Migrations/20250414120338_CarModelAbrv.Designer.cs
@@ -4,6 +4,7 @@ using CarsProject.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarsProject.DAL.Migrations
 {
     [DbContext(typeof(CarsDbContext))]
-    partial class CarsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250414120338_CarModelAbrv")]
+    partial class CarModelAbrv
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarsProject.DAL/Migrations/20250414120338_CarModelAbrv.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarsProject.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CarModelAbrv : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Abrv",
+                table: "CarModels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Abrv",
+                table: "CarModels");
+        }
+    }
+}
