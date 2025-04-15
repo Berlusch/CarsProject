b192803a81f295dc53687b82717abe36da2da1f3
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-15 15:18:44 +0200

    Update CarOwnerDTORead.cs

--- CarsProject.Model/DTO/CarOwnerDTORead.cs
+++ CarsProject.Model/DTO/CarOwnerDTORead.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarsProject.Model.DTO
+﻿namespace CarsProject.Model.DTO
 {
     public record CarOwnerDTORead
     (
@@ -14,4 +8,5 @@ namespace CarsProject.Model.DTO
         DateOnly DateOfBirth
 
     );
+    
 }
