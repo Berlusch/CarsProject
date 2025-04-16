9c7e24ceecb87f8d0ebb00846b49bf1eaed8e35c
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-16 16:40:17 +0200

    Testing table layout

--- carsproject-frontend/src/pages/CarMakes/CarMakesList.jsx
+++ carsproject-frontend/src/pages/CarMakes/CarMakesList.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import Table from '../../components/Table.jsx';
 
-function CarMakesList() {
-    return <h1>Car Makes List Page</h1>;
-  }
-  
-  export default CarMakesList;
+
+const columns = [
+  { header: 'ID', accessor: 'id' },
+  { header: 'Marka', accessor: 'make' },
+  { header: 'Model', accessor: 'model' },
+  { header: 'Godina', accessor: 'year' }
+];
+
+const data = [
+  { id: 1, make: 'Toyota', model: 'Corolla', year: 2020 },
+  { id: 2, make: 'Volkswagen', model: 'Golf', year: 2019 },
+  { id: 3, make: 'Ford', model: 'Focus', year: 2021 },
+  { id: 4, make: 'BMW', model: 'X5', year: 2022 }
+];
+
+const Cars = () => {
+  return (
+    <div>
+      <h2>Cars List</h2>
+      <Table columns={columns} data={data} />
+    </div>
+  );
+};
+
+export default Cars;
