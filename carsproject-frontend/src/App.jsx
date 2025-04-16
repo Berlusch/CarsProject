c39b23e19343d48e9a0cad3f0108bd3a9ab33fcf
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-16 16:39:55 +0200

    Update App.jsx

--- carsproject-frontend/src/App.jsx
+++ carsproject-frontend/src/App.jsx
@@ -1,37 +1,21 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home'
-
-
-
-
+import Home from './pages/Home';
+import MainLayout from './layouts/MainLayout';
+import CarMakesList from './pages/CarMakes/CarMakesList';
+import { RouteNames } from './common/constants'
 
 function App() {
   return (
-    
     <Router>
-    <div className="app">
-      {}
-      <header>
-      <a href="/">
-        <img src="/cars-logo.png" alt="Logo" className="logo" />
-        </a>
-      </header>
-
-      <main className="main">
-        <Routes>
-          {}
-          <Route path="/" element={<Home />} />                                         
-        </Routes>
-        <div style={{ textAlign: 'center', paddingTop: '40px', paddingRight: '30px', fontSize: '12px' }}>
-  &copy; Bernarda Lusch 2025
-</div>
-      </main>
-    </div>
-     
-  </Router>
-    
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path={RouteNames.CAR_MAKE_LIST} element={<CarMakesList />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
