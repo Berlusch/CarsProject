fc63ec079d9e92b4c98e3d4093c3dfe6351ee068
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-17 16:22:02 +0200

    Update CarMakeService.js

---
+++ carsproject-frontend/src/common/Services/CarMakeService.js
@@ -0,0 +1,53 @@
+import { HttpService } from "./HttpService"; 
+
+async function getPSF(page = 1, pageSize = 5, sort = "name", filter = "") {
+  try {
+    const response = await HttpService.get("/CarMake/getPfs", {
+      params: { pageNumber: page, pageSize, sortBy: sort, filter },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Fetching data error:", error);
+    throw error;
+  }
+}
+
+async function getById(id){
+    return await HttpService.get('/CarMake/'+id)
+    .then((response)=>{
+        //console.table(response.data)
+        return {error:false, message: response.data};
+    })
+    .catch((e)=>{})
+}
+
+async function add(CarMake){
+    return await HttpService.post('/CarMake', CarMake)
+    .then(()=>{return{error:false, message: 'Added'}})
+    .catch(()=>{return{error:true, message:'Adding problem'}})
+}
+
+async function edit(id,CarMake){
+    return await HttpService.put('/CarMake/'+id, CarMake)
+    .then(()=>{return{error:false, message: 'Edited'}})
+    .catch(()=>{return{error:true, message:'Editing problem'}})
+}
+
+async function remove(id,CarMake){
+    return await HttpService.delete('/CarMake/'+id, CarMake)
+    .then(()=>{return{error:false, message: 'Removed'}})
+    .catch(()=>{return{error:true, message:'Operation failed'}})
+}
+
+  
+  export default{
+    getPSF,
+    getById,
+    add,
+    edit,
+    remove    
+}
+
+
+
+
