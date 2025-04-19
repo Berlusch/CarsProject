e22031a04195b73bc32e6a61c0d9f9246379e625
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-19 18:48:15 +0200

    Update App.jsx

--- carsproject-frontend/src/App.jsx
+++ carsproject-frontend/src/App.jsx
@@ -7,6 +7,10 @@ import CarMakesList from './pages/CarMakes/CarMakesList';
 import { RouteNames } from './common/constants';
 import CarMakesAdd from './pages/CarMakes/CarMakesAdd';
 import CarMakesEdit from './pages/CarMakes/CarMakesEdit';
+import CarEngineTypesList from './pages/CarEngineTypes/CarEngineTypesList';
+import CarRegistrationsList from './pages/CarRegistrations/CarRegistrationsList';
+import CarRegistrationsAdd from './pages/CarRegistrations/CarRegistrationsAdd';
+import CarRegistrationsEdit from './pages/CarRegistrations/CarRegistrationsEdit';
 
 
 function App() {
@@ -14,10 +18,19 @@ function App() {
     <Router>
       <Routes>
         <Route element={<MainLayout />}>
+
           <Route path="/" element={<Home />} />
+
           <Route path={RouteNames.CAR_MAKE_LIST} element={<CarMakesList />} />
           <Route path={RouteNames.CAR_MAKE_ADD} element={<CarMakesAdd />} />
           <Route path={RouteNames.CAR_MAKE_EDIT} element={<CarMakesEdit/>} />
+
+          <Route path={RouteNames.CAR_REGISTRATION_LIST} element={<CarRegistrationsList />} />
+          <Route path={RouteNames.CAR_REGISTRATION_ADD} element={<CarRegistrationsAdd />} />
+          <Route path={RouteNames.CAR_REGISTRATION_EDIT} element={<CarRegistrationsEdit/>} />
+
+          <Route path={RouteNames.CAR_ENGINE_TYPE_LIST} element={<CarEngineTypesList/>} />
+          
         </Route>
       </Routes>
     </Router>
