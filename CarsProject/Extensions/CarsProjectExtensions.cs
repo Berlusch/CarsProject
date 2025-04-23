22b6881491e7a3627e7135f464d8826f724e7c89
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-04-23 20:52:43 +0200

    CORS configuration

---
+++ CarsProject/Extensions/CarsProjectExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace CarsProject.Extensions
+{
+    public static class CarsProjectExtensions
+    {
+        public static void AddCarsProjectCORS(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder =>
+                        builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                );
+            });
+        }
+    }
+}
+
+
+    
+    
