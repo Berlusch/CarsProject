e7db614f266d48105046443f475ffa8fbbc954fd
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-01-27 12:26:38 +0100

    Update IGenericRepository.cs

--- CarsProject.Repository.Common/IGenericRepository.cs
+++ CarsProject.Repository.Common/IGenericRepository.cs
@@ -4,15 +4,13 @@ namespace CarsProject.Repository.Common
 {
     public interface IGenericRepository<T> where T : class
     {
-        
         IQueryable<T> GetQuery(PSFParameters parameters);
-
+        
         Task<T> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
         Task<T> UpdateAsync(T entity);
         Task<bool> DeleteAsync(int id);
     }
 }
-
 
 
