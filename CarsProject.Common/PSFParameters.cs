b60d409e2a047010f200ce49c7f986cc704e738e
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-01-27 10:39:19 +0100

    Create PSFParameters.cs

---
+++ CarsProject.Common/PSFParameters.cs
@@ -0,0 +1,9 @@
+﻿namespace CarsProject.Common
+{
+    public class PSFParameters
+    {
+        public PagingParameters Paging { get; set; } = new();
+        public SortingParameters Sorting { get; set; } = new();
+        public FilterParameters Filter { get; set; } = new();
+    }
+}
