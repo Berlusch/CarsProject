05da7f72c4dc0313c72e471e0947673250781102
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-01-27 11:15:21 +0100

    Update GenericRepository.cs

--- CarsProject.Repository/GenericRepository.cs
+++ CarsProject.Repository/GenericRepository.cs
@@ -1,108 +1,90 @@
-﻿using CarsProject.DAL;
+﻿using CarsProject.Common;
+using CarsProject.DAL;
 using CarsProject.Repository.Common;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace CarsProject.Repository
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
-        private readonly CarsDbContext _context;
-        private readonly DbSet<T> _dbSet;
+        protected readonly CarsDbContext _context;
+        protected readonly IUnitOfWork _unitOfWork;
+        protected readonly DbSet<T> _dbSet;
 
-        public GenericRepository(CarsDbContext context)
+        public GenericRepository(CarsDbContext context, IUnitOfWork unitOfWork)
         {
             _context = context;
+            _unitOfWork = unitOfWork;
             _dbSet = context.Set<T>();
         }
 
-        
-        public async Task<PagedResult<T>> GetPagedAsync(
-            int pageNumber,
-            int pageSize,
-            Func<IQueryable<T>, IQueryable<T>>? filter = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null)
+        public IQueryable<T> GetQuery(PSFParameters parameters)
         {
             IQueryable<T> query = _dbSet;
 
-           
-            if (filter != null)
+            if (!string.IsNullOrEmpty(parameters.Filter.Filter) &&
+                !string.IsNullOrEmpty(parameters.Filter.PropertyName))
             {
-                query = filter(query);
+                var propInfo = typeof(T).GetProperty(parameters.Filter.PropertyName,
+                    BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                if (propInfo != null && propInfo.PropertyType == typeof(string))
+                {
+                    query = query.Where(e =>
+                        EF.Property<string>(e, parameters.Filter.PropertyName)
+                          .Contains(parameters.Filter.Filter));
+                }
             }
 
-            
-            if (orderBy != null)
+            if (!string.IsNullOrEmpty(parameters.Sorting.OrderBy))
             {
-                query = orderBy(query);
+                var propInfo = typeof(T).GetProperty(parameters.Sorting.OrderBy,
+                    BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                if (propInfo != null)
+                {
+                    query = parameters.Sorting.Descending
+                        ? query.OrderByDescending(x => propInfo.GetValue(x, null))
+                        : query.OrderBy(x => propInfo.GetValue(x, null));
+                }
             }
 
-           
-            var totalItems = await query.CountAsync();
-
-            
-            var items = await query
-                .Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize)
-                .ToListAsync();
-
-            return new PagedResult<T>
-            {
-                TotalCount = totalItems,  
-                Items = items              
-            };
+            return query;
         }
-        
-        public virtual async Task<T> GetByIdAsync(int id)
+
+        public async Task<T> GetByIdAsync(int id)
         {
             var entity = await _dbSet.FindAsync(id);
             if (entity == null)
-            {
                 throw new KeyNotFoundException($"Entity with ID {id} not found.");
-            }
-
             return entity;
         }
 
-        
         public async Task<T> AddAsync(T entity)
         {
             await _dbSet.AddAsync(entity);
-            await _context.SaveChangesAsync();
+            await _unitOfWork.SaveChangesAsync();
             return entity;
         }
 
-       
         public async Task<T> UpdateAsync(T entity)
         {
             _dbSet.Update(entity);
-            await _context.SaveChangesAsync();
+            await _unitOfWork.SaveChangesAsync();
             return entity;
         }
 
-        
         public async Task<bool> DeleteAsync(int id)
         {
             var entity = await _dbSet.FindAsync(id);
             if (entity != null)
             {
                 _dbSet.Remove(entity);
-                await _context.SaveChangesAsync();
+                await _unitOfWork.SaveChangesAsync();
                 return true;
             }
             return false;
         }
-
-        Task<WebApi.PagedResult<T>> IGenericRepository<T>.GetPagedAsync
-            (int pageNumber, int pageSize, Func<IQueryable<T>, IQueryable<T>>? filter, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    
-    public class PagedResult<T>
-    {
-        public int TotalCount { get; set; }  
-        public required IEnumerable<T> Items { get; set; } 
     }
 }
