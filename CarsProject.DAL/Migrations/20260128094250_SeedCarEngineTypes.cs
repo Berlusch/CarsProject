28457351e1b81fc4b2757fa60d3eebce939733cf
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-01-28 10:52:54 +0100

    Database migration for seed data (CarEngineTypes)

---
+++ CarsProject.DAL/Migrations/20260128094250_SeedCarEngineTypes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarsProject.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCarEngineTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CarEngineType",
+                columns: new[] { "Id", "Abrv", "Type" },
+                values: new object[,]
+                {
+                    { 1, "PET", "Petrol" },
+                    { 2, "DSL", "Diesel" },
+                    { 3, "ELE", "Electric" },
+                    { 4, "HYB", "Hybrid" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CarEngineType",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CarEngineType",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CarEngineType",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CarEngineType",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
