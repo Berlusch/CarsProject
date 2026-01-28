15f3be4d1eec9a2e9a0eb89c7825d6e919146891
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-01-28 18:56:21 +0100

    Remove unnecessary reference

--- CarsProject.DAL/CarsDBContext.cs
+++ CarsProject.DAL/CarsDBContext.cs
@@ -4,11 +4,11 @@ namespace CarsProject.DAL
 {
     public class CarsDbContext : DbContext
     {
-        public DbSet<CarsProject.Model.CarMake> CarMakes { get; set; }
-        public DbSet<CarsProject.Model.CarModel> CarModels { get; set; }
-        public DbSet<CarsProject.Model.CarOwner> CarOwners { get; set; }
-        public DbSet<CarsProject.Model.CarRegistration> CarRegistrations { get; set; }
-        public DbSet<CarsProject.Model.CarEngineType> CarEngineTypes { get; set; }
+        public DbSet<Model.CarMake> CarMakes { get; set; }
+        public DbSet<Model.CarModel> CarModels { get; set; }
+        public DbSet<Model.CarOwner> CarOwners { get; set; }
+        public DbSet<Model.CarRegistration> CarRegistrations { get; set; }
+        public DbSet<Model.CarEngineType> CarEngineTypes { get; set; }
 
         public CarsDbContext(DbContextOptions<CarsDbContext> options) : base(options) { }
 
