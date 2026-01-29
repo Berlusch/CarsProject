dc1c62f08bcb92bcbb9ee74b871486f33194f0a4
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-01-29 12:32:00 +0100

    Update CarModel.cs

--- CarsProject.Model/CarModel.cs
+++ CarsProject.Model/CarModel.cs
@@ -4,19 +4,21 @@ namespace CarsProject.Model
 {
     public class CarModel : EntityBase, ICarModel
     {
-        
         public string Name { get; set; } = "";
-        public string Abrv { get; set; } = ""; 
-        public required CarEngineType CarEngineType { get; set; }                
-        public required CarMake CarMake { get; set; }
-
+        public string Abrv { get; set; } = "";
+        
+        public int CarMakeId { get; set; }
+        public int CarEngineTypeId { get; set; }
+        
+        public virtual CarMake CarMake { get; set; } = null!;
+        public virtual CarEngineType CarEngineType { get; set; } = null!;
         
         ICarEngineType ICarModel.CarEngineType
         {
             get => CarEngineType;
             set => CarEngineType = (CarEngineType)value;
         }
-                
+
         ICarMake ICarModel.CarMake
         {
             get => CarMake;
@@ -24,3 +26,4 @@ namespace CarsProject.Model
         }
     }
 }
+
