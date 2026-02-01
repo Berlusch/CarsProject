5943bc4d7f698e96b14552ea002d54355738264c
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-02-01 10:30:19 +0100

    Remove override methods with .Include

--- CarsProject.Repository/CarModelRepository.cs
+++ CarsProject.Repository/CarModelRepository.cs
@@ -1,48 +1,14 @@
-﻿using CarsProject.Common;
-using CarsProject.DAL;
+﻿using CarsProject.DAL;
 using CarsProject.Model;
 using CarsProject.Repository.Common;
-using Microsoft.EntityFrameworkCore;
 
 namespace CarsProject.Repository
 {
     public class CarModelRepository : GenericRepository<CarModel>, ICarModelRepository
     {
-        public CarModelRepository(CarsDbContext context)
-            : base(context)
+        public CarModelRepository(CarsDbContext context) : base(context)
         {
         }
-
-        public override IQueryable<CarModel> GetQuery(PSFParameters parameters)
-        {
-            IQueryable<CarModel> query = _dbSet
-                .Include(cm => cm.CarMake)
-                .Include(cm => cm.CarEngineType);
-            
-
-            return query;
-        }
-
-        public override async Task<CarModel> GetByIdAsync(int id)
-        {
-            var carModel = await _dbSet
-                .Include(cm => cm.CarMake)
-                .Include(cm => cm.CarEngineType)
-                .FirstOrDefaultAsync(cm => cm.Id == id);            
-
-
-            if (carModel == null)
-            {
-                throw new KeyNotFoundException($"CarModel with ID {id} not found.");
-            }
-                        
-
-            return carModel;
-        }
+        
     }
 }
-
-
-
-
-
