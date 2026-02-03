2b247f6a7ca9e201c515e018f24011a49f8cb283
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-02-03 09:53:18 +0100

    Create PagedResult class in Project:Common

---
+++ CarsProject.Common/PagedResult.cs
@@ -0,0 +1,12 @@
+﻿namespace CarsProject.Common
+{
+    public class PagedResult<T>
+    {
+        public required IReadOnlyList<T> Items { get; init; }
+        public required int TotalCount { get; init; }
+        public required PagingParameters Paging { get; init; }
+
+        public int TotalPages =>
+            (int)Math.Ceiling((double)TotalCount / Paging.PageSize);
+    }
+}
