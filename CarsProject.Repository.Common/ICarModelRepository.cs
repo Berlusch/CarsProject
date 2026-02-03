513f29fe9c19162993fcef9ae6ab615e200a2d7b
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-02-03 11:55:01 +0100

    Add PagedResult to entity repository and interface

--- CarsProject.Repository.Common/ICarModelRepository.cs
+++ CarsProject.Repository.Common/ICarModelRepository.cs
@@ -1,9 +1,10 @@
-﻿using CarsProject.Model;
+﻿using CarsProject.Common;
+using CarsProject.Model;
 
 namespace CarsProject.Repository.Common
 {
     public interface ICarModelRepository : IGenericRepository<CarModel>
     {
-        
+        Task<PagedResult<CarModel>> GetPagedAsync(PFSParameters? parameters = null);
     }
 }
--- CarsProject.Repository/CarModelRepository.cs
+++ CarsProject.Repository/CarModelRepository.cs
@@ -1,14 +1,37 @@
-﻿using CarsProject.DAL;
+﻿using CarsProject.Common;
+using CarsProject.DAL;
 using CarsProject.Model;
 using CarsProject.Repository.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarsProject.Repository
 {
-    public class CarModelRepository : GenericRepository<CarModel>, ICarModelRepository
-    {
-        public CarModelRepository(CarsDbContext context) : base(context)
+    public class CarModelRepository(CarsDbContext context)
+    : GenericRepository<CarModel>(context), ICarModelRepository
+    {   
+
+        public async Task<PagedResult<CarModel>> GetPagedAsync(PFSParameters? parameters = null)
         {
+            parameters ??= new PFSParameters();
+            parameters.Paging ??= new PagingParameters();
+       
+            parameters.Paging.PageSize = parameters.Paging.PageSize != 0 ? parameters.Paging.PageSize : 5;
+
+            var query = GetQuery(parameters);
+
+            var totalCount = await query.CountAsync();
+
+            var items = await query
+                .Skip(parameters.Paging.Skip)
+                .Take(parameters.Paging.PageSize)
+                .ToListAsync();
+
+            return new PagedResult<CarModel>
+            {
+                Items = items,
+                TotalCount = totalCount,
+                Paging = parameters.Paging
+            };
         }
-        
     }
 }
