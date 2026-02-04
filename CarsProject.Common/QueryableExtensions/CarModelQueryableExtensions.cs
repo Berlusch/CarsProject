65915f4bd2deebf1fcc686a77a9818c196011fe3
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-02-04 12:39:37 +0100

    add IncludeFKs extensions for CarModel and CarRegistration

---
+++ CarsProject.Common/QueryableExtensions/CarModelQueryableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CarsProject.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarsProject.Common.QueryableExtensions
+{
+    public static class CarModelQueryableExtensions
+    {
+        public static IQueryable<CarModel> IncludeFKs(this IQueryable<CarModel> query)
+        {
+            return query
+                .Include(cm => cm.CarMake)
+                .Include(cm => cm.CarEngineType);
+        }
+    }
+}
---
+++ CarsProject.Common/QueryableExtensions/CarRegistrationQueryableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CarsProject.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarsProject.Common.QueryableExtensions
+{
+    public static class CarRegistrationQueryableExtensions
+    {
+        public static IQueryable<CarRegistration> IncludeFKs(this IQueryable<CarRegistration> query)
+        {
+            return query
+                .Include(cm => cm.CarOwner)
+                .Include(cm => cm.CarModel);
+        }
+    }
+}
