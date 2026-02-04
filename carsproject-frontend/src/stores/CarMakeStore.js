494c37a89587b1cbd927f9460a0c8cbbf03949b5
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-02-04 14:01:05 +0100

    Update CarMakeStore.js

--- carsproject-frontend/src/stores/CarMakeStore.js
+++ carsproject-frontend/src/stores/CarMakeStore.js
@@ -1,5 +1,5 @@
-import { makeAutoObservable } from "mobx";
 import CarMakeService from "../common/Services/CarMakeService";
+import { makeAutoObservable, runInAction } from "mobx";
 
 
 class CarMakeStore {
@@ -36,7 +36,29 @@ class CarMakeStore {
       hasNextPage: this.hasNextPage   
     };
   }
-  
+
+  async fetchCarMakes() {
+  try {
+    const pfs = {
+      currentPage: this.currentPage,
+      pageSize: this.pageSize,
+      searchTerm: this.searchTerm
+    };
+
+    const response = await CarMakeService.getCarMakesPFS(pfs);
+
+    runInAction(() => {
+      this.carMakes = response.items;
+      this.hasNextPage = response.hasNextPage;
+    });
+
+  } catch (error) {
+    runInAction(() => {
+      this.error = "Error fetching car makes";
+    });
+    console.error(error);
+  }
+}
   async addCarMake(name, abrv) {
     this.addStatus = { error: false, message: 'Adding car make...' }; 
   
