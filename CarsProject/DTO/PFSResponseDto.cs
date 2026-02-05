63b05152f95eee36d1881311d85be5e3481014d5
Bernarda Lusch <bernarda.lusch@gmail.com>
2026-02-05 09:26:31 +0100

    Create PFSResponseDto.cs

---
+++ CarsProject/DTO/PFSResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarsProject.WebApi.DTO
+{
+    
+        public class PFSResponseDto<T>
+        {
+            public IEnumerable<T> Items { get; set; } = new List<T>();
+            public bool HasNextPage { get; set; }
+            public int TotalCount { get; set; }
+        }
+    
+}
